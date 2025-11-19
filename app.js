adb908e749814c630bf26f36e8b73f4bd1760e0e
SayyadDBvertex <nodedb9@gmail.com>
2025-11-19 11:30:44 +0530

    dhdshs

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ const app = express();
 connectDB();
 dotenv.config();
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
